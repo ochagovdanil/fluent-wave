de6ad18da942322f0bdfc04779e45e4fc1d82a75
Danil Ochagov <danilochagov@gmail.com>
2023-10-03 17:06:20 +0300

    Fix errors with path alias

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,11 @@ import path from 'path';
 export default defineConfig({
 	resolve: {
 		alias: {
-			'@assets': path.resolve(__dirname, './src/assets'),
-			'@styles': path.resolve(__dirname, './src/styles'),
-			'@data': path.resolve(__dirname, './src/data'),
-			'@hooks': path.resolve(__dirname, './src/hooks'),
-			'@components': path.resolve(__dirname, './src/components'),
+			'@assets': path.resolve(__dirname, './src/assets/'),
+			'@styles': path.resolve(__dirname, './src/styles/'),
+			'@data': path.resolve(__dirname, './src/data/'),
+			'@hooks': path.resolve(__dirname, './src/hooks/'),
+			'@components': path.resolve(__dirname, './src/components/'),
 		},
 	},
 	plugins: [react()],
